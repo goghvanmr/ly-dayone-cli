80f9e53a054d25d45fc64e9ea1c243f7cb0bb5f4
goghvanmr <eagle.liyi@gmail.com>
2013-08-05 22:31:30 +0800

    add argparse and do refactoring work

--- dayone.py
+++ dayone.py
@@ -1,35 +1,12 @@
 # -*- coding: utf-8 -*-
+import argparse
+import reminder
 
-import plistlib
-import os
-import getpass
-import Foundation
+parser = argparse.ArgumentParser()
+parser.add_argument('-r', '--reminder', help = 'Export TODO entires to reminder.app', action= 'store_true')
+args = parser.parse_args()
 
-def parse_dayone():
-	username = getpass.getuser()
-	entry_folder_path = '/Users/' + username + '/Library/Mobile Documents/5U8NS4GX82~com~dayoneapp~dayone/Documents/Journal_dayone/entries/'
-
-	for root, dirs, files in os.walk(entry_folder_path):
-		for f in files:
-			pl = plistlib.readPlist(root + f)
-
-			if 'Tags' not in pl: continue
-
-			if 'TODO' in pl['Tags']:
-				entry_text = pl['Entry Text'].replace('"', '\\"')
-
-				reminder_name = entry_text.splitlines()[0] 
-				reminder_body = entry_text 
-				reminder_list = 'TODO'
-
-				apple_script_command = """
-					tell application "Reminders" \n
-						make new reminder with properties {body:"%s", name:"%s", container:list "%s"} \n
-					end tell
-				""" % (reminder_body, reminder_name, reminder_list)
-
-				s = Foundation.NSAppleScript.alloc().initWithSource_(apple_script_command)
-				s.executeAndReturnError_(None)
-
-if __name__ == '__main__':
-	parse_dayone()
+if args.reminder:
+	reminder.export_todo_to_reminder()
+else:
+	parser.print_help()
---
+++ reminder.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+import plistlib
+import os
+import getpass
+import Foundation
+
+from config import *
+
+JOURNAL_ENTRIES_PATH = '/Library/Mobile Documents/5U8NS4GX82~com~dayoneapp~dayone/Documents/Journal_dayone/entries/' 
+#JOURNAL_ENTRIES_PATH = '/Desktop/entries/' 
+
+def export_todo_to_reminder():
+	username = getpass.getuser()
+	entry_folder_path = '/Users/' + username + JOURNAL_ENTRIES_PATH  
+
+	for root, dirs, files in os.walk(entry_folder_path):
+		for f in files:
+			plist_file = root + f
+
+			if not plist_file.endswith('.doentry'): continue 
+
+			pl = plistlib.readPlist(plist_file)
+
+			if 'Tags' not in pl: continue
+
+			if TAG_TO_EXPORT in pl['Tags']:
+				entry_text = pl['Entry Text'].replace('"', '\\"')
+
+				reminder_name = entry_text.splitlines()[0] 
+				reminder_body = entry_text 
+				reminder_list = REMINDER_LIST
+
+				reminder_command_template = open('reminder.template').read()
+				reminder_command = reminder_command_template % (reminder_list, reminder_list, reminder_body, reminder_name, reminder_list)
+
+				s = Foundation.NSAppleScript.alloc().initWithSource_(reminder_command)
+				s.executeAndReturnError_(None)
+
+				pl['Tags'].remove(TAG_TO_EXPORT)
+				pl['Tags'].append(TAG_AFTER_EXPORT)
+				plistlib.writePlist(pl, plist_file)
+
+if __name__ == '__main__':
+	export_todo_to_reminder()

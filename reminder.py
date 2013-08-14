0dcfc40802fd2b5962194c42064c769f33ea2f30
goghvanmr <eagle.liyi@gmail.com>
2013-08-14 21:39:26 +0800

    remove tag from entry and refactor some code

--- reminder.py
+++ reminder.py
@@ -17,7 +17,7 @@ def export_todo_to_reminder():
 		for f in files:
 			plist_file = root + f
 
-			if not plist_file.endswith('.doentry'): continue
+			if not dayone_entry(plist_file): continue
 
 			pl = plistlib.readPlist(plist_file)
 
@@ -26,19 +26,37 @@ def export_todo_to_reminder():
 			if TAG_TO_EXPORT in pl['Tags']:
 				entry_text = pl['Entry Text'].replace('"', '\\"')
 
-				reminder_name = entry_text.splitlines()[0]
-				reminder_body = entry_text
-				reminder_list = REMINDER_LIST
+				reminder_command = create_apple_script_command_from(entry_text)
+				execute_apple_script_command(reminder_command)
 
-				reminder_command_template = open('reminder.template').read()
-				reminder_command = reminder_command_template % (reminder_list, reminder_list, reminder_body, reminder_name, reminder_list)
+				save_plist(pl, plist_file)
 
-				s = Foundation.NSAppleScript.alloc().initWithSource_(reminder_command)
-				s.executeAndReturnError_(None)
 
-				pl['Tags'].remove(TAG_TO_EXPORT)
-				pl['Tags'].append(TAG_AFTER_EXPORT)
-				plistlib.writePlist(pl, plist_file)
+def dayone_entry(plist_file):
+	return plist_file.endswith('.doentry')
+
+def create_apple_script_command_from(entry_text):
+	reminder_name = entry_text.splitlines()[0]
+	reminder_body = entry_text
+	reminder_list = REMINDER_LIST
+
+	reminder_command_template = open('reminder.template').read()
+	reminder_command = reminder_command_template % (reminder_list, reminder_list, reminder_body, reminder_name, reminder_list)
+
+	return reminder_command
+
+def execute_apple_script_command(command):
+	s = Foundation.NSAppleScript.alloc().initWithSource_(command)
+	s.executeAndReturnError_(None)
+
+def save_plist(plist, plist_file):
+	plist['Tags'].remove(TAG_TO_EXPORT)
+	plist['Tags'].append(TAG_AFTER_EXPORT)
+
+	hash_tag = '#%s' % TAG_TO_EXPORT
+	plist['Entry Text'] = plist['Entry Text'].replace(hash_tag, '')
+
+	plistlib.writePlist(plist, plist_file)
 
 if __name__ == '__main__':
 	export_todo_to_reminder()

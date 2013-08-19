c9b292eecb7ea9caa44d75e764cef903d1f0b46a
goghvanmr <eagle.liyi@gmail.com>
2013-08-19 17:59:43 +0800

    question and score

--- zen.py
+++ zen.py
@@ -1,7 +1,26 @@
 # -*- coding: utf-8 -*-
 
+import plistlib
+
 def practice_zen():
-    print 'zen practice'
+    question_plist = plistlib.readPlist('test.question')
+
+    question = question_plist['question']
+    bonus = question_plist['bonus']
+    penalty = question_plist['penalty']
+
+    answer = raw_input('%s (y/n)' % question)
+
+    score = plistlib.readPlist('total.score')['score']
+
+    if answer is 'y' or answer is '':
+        score += bonus
+    elif answer is 'n':
+        score += penalty
+    else:
+        print 'wrong input: %s' % answer
+
+    print score
 
 if __name__ == '__main__':
     practice_zen()
